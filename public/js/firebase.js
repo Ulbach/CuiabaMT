f826ed9ac1a0ab21e9c9b26b58b9186914892083
Ulbach <124060988+Ulbach@users.noreply.github.com>
2026-05-28 14:12:12 -0400

    Atualiza visual e corrige acentos do CuiabaMT

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -3,9 +3,9 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebas
 
 const firebaseConfig = {
   apiKey: "AIzaSyBeG7uirpSS3N0I48fy7WqJa_SSrb8A-48",
-  authDomain: "delta-pro-cuiaba.firebaseapp.com",
-  projectId: "delta-pro-cuiaba",
-  storageBucket: "delta-pro-cuiaba.appspot.com",
+  authDomain: "cuiaba-01617931-f126e.firebaseapp.com",
+  projectId: "cuiaba-01617931-f126e",
+  storageBucket: "cuiaba-01617931-f126e.firebasestorage.app",
   messagingSenderId: "797115279316",
   appId: "1:797115279316:web:f229366de16d4e066e1841"
 };
